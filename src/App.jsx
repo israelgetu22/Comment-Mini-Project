81f7decaf9691340653f4c0a71cd6c0ef2074f0d
Isarel Getu <israelgetu101@gmail.com>
2022-04-27 01:15:59 -0500

    Good

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+//import faker from "faker";
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avator">
+          <img
+            style={{ width: "50px" }}
+            alt="avator"
+            src="https://www.at-languagesolutions.com/en/wp-content/uploads/2016/06/http-1.jpg"
+          />
+          {/* <img alt="avator" src={faker.image.avatar()} /> */}
+        </a>
+        <div className="content">
+          <a href="" className="author">
+            Israel
+          </a>
+          <div className="metadata">
+            <span className="data"> Today at 7:00 </span>
+          </div>
+          <div className="text">Nice Blog POST!!!</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
